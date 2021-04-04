dbcad09a8e3394739c54ddc6afebc1495d214f00
Alex Sandri <me@alexsandri.com>
2021-04-04 20:18:28 +0200

    Show map

--- src/app/home/signed-in/signed-in.component.ts
+++ src/app/home/signed-in/signed-in.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from "leaflet";
 
 @Component({
   selector: 'signed-in-home',
   templateUrl: './signed-in.component.html',
   styleUrls: ['./signed-in.component.scss']
 })
-export class SignedInHomeComponent
+export class SignedInHomeComponent implements AfterViewInit
 {
+  private map?: L.Map;
+
   constructor()
   {}
+
+  public ngAfterViewInit()
+  {
+    this.initMap();
+  }
+
+  private initMap()
+  {
+    this.map = L.map("map", {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3,
+    });
+
+    const tiles = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>",
+    });
+
+    tiles.addTo(this.map);
+  }
 }
