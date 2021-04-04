b9054e72c1dc8b411ca0e0fcdd17072f373a3cf6
Alex Sandri <me@alexsandri.com>
2021-04-04 17:55:19 +0200

    Update header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HeaderComponent
+{
+  constructor(public auth: AuthService, public router: Router)
+  {}
 }
