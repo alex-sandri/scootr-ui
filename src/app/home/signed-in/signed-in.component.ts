1ef0d9eaa5a2b85ed57d5f9d0aa85e38cac488e0
Alex Sandri <me@alexsandri.com>
2021-04-04 20:25:05 +0200

    Default center to Rome and increase maxZoom

--- src/app/home/signed-in/signed-in.component.ts
+++ src/app/home/signed-in/signed-in.component.ts
@@ -21,12 +21,15 @@ export class SignedInHomeComponent implements AfterViewInit
   private initMap()
   {
     this.map = L.map("map", {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3,
+      center: [
+        41.9027835,
+        12.4963655,
+      ],
+      zoom: 19,
     });
 
     const tiles = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 18,
+      maxZoom: 19,
       minZoom: 3,
       attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>",
     });
