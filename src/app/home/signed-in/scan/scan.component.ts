ca17c0c0ea959dc9acf16798de744258ad0c2d71
Alex Sandri <me@alexsandri.com>
2021-04-09 15:26:12 +0200

    Start implementing qr code scanner

--- src/app/home/signed-in/scan/scan.component.ts
+++ src/app/home/signed-in/scan/scan.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-scan',
   templateUrl: './scan.component.html',
   styleUrls: ['./scan.component.scss']
 })
-export class ScanComponent
+export class ScanComponent implements AfterViewInit
 {
+  public hasCamera = true;
+
   constructor()
   {}
+
+  public ngAfterViewInit()
+  {
+    navigator
+      .mediaDevices
+      .getUserMedia({
+        video: {
+          facingMode: "environment",
+        },
+      })
+      .then(stream =>
+      {
+        // TODO
+      })
+      .catch(() =>
+      {
+        this.hasCamera = false;
+      });
+  }
 }
