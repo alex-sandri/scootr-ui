1acde495786bc0216112f060c4aded8a6036f316
Alex Sandri <me@alexsandri.com>
2021-04-11 09:20:16 +0200

    Implement wallet menu

--- src/app/settings/wallets/wallet/menu/menu.component.ts
+++ src/app/settings/wallets/wallet/menu/menu.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService, IWallet } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'wallet-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class WalletMenuComponent
+export class WalletMenuComponent implements OnInit
 {
-  constructor()
+  @Input("section")
+  public section?: string;
+
+  public wallet?: IWallet;
+
+  constructor(private api: ApiService, private route: ActivatedRoute)
   {}
+
+  public ngOnInit()
+  {
+    this.route.params
+      .subscribe({
+        next: params =>
+        {
+          this.api
+            .retrieveWallet(params.id)
+            .then(response =>
+            {
+              this.wallet = response.data;
+            });
+        },
+      });
+  }
 }
