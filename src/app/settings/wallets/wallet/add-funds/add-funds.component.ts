817d9b369f39b10dee159a4e957d7c5f4fef4ff2
Alex Sandri <me@alexsandri.com>
2021-04-11 18:39:28 +0200

    Implement add funds component

--- src/app/settings/wallets/wallet/add-funds/add-funds.component.ts
+++ src/app/settings/wallets/wallet/add-funds/add-funds.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-add-funds',
   templateUrl: './add-funds.component.html',
   styleUrls: ['./add-funds.component.scss']
 })
-export class AddFundsComponent implements OnInit {
+export class AddFundsComponent implements OnInit
+{
+  public walletId?: string;
 
-  constructor() { }
+  public form = new FormGroup({
+    amount: new FormControl(),
+  });
 
-  ngOnInit(): void {
+  constructor(private api: ApiService, private router: Router, private route: ActivatedRoute)
+  {}
+
+  public ngOnInit()
+  {
+    this.route.params
+      .subscribe({
+        next: params =>
+        {
+          this.walletId = params.id;
+        },
+      });
   }
 
+  public async onSubmit(e: Event)
+  {
+    e.preventDefault();
+
+    if (!this.walletId)
+    {
+      return;
+    }
+
+    const response = await this.api.addFundsToWallet(
+      this.form.get("amount")?.value ?? 0,
+      this.walletId,
+    );
+
+    Object.entries(this.form.controls).forEach(([ name, control ]) =>
+    {
+      control.setErrors({
+        errors: response.errors?.filter(e => e.field === name)
+      });
+    });
+
+    if (response.success)
+    {
+      this.router.navigate([ ".." ], {
+        relativeTo: this.route,
+      });
+    }
+  }
 }
