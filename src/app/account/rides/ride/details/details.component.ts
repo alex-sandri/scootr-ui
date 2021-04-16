7ed1760771bcec1a908f6ff39c579ec0f9448a1c
Alex Sandri <me@alexsandri.com>
2021-04-16 09:05:28 +0200

    Show ride details

--- src/app/account/rides/ride/details/details.component.ts
+++ src/app/account/rides/ride/details/details.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService, IRide } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-ride-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class RideDetailsComponent
+export class RideDetailsComponent implements OnInit
 {
-  constructor()
+  public ride?: IRide;
+
+  constructor(private api: ApiService, private route: ActivatedRoute)
   {}
+
+  public ngOnInit()
+  {
+    this.route.params.subscribe({
+      next: async params =>
+      {
+        const response = await this.api.retrieveRide(params.id);
+
+        this.ride = response.data;
+      },
+    });
+  }
 }
