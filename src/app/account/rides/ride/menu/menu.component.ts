e6fe1554e08a8c6d1107b8e86c39908a168be730
Alex Sandri <me@alexsandri.com>
2021-04-16 08:58:18 +0200

    Implement ride menu

--- src/app/account/rides/ride/menu/menu.component.ts
+++ src/app/account/rides/ride/menu/menu.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'ride-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class RideMenuComponent
+export class RideMenuComponent implements OnInit
 {
-  constructor()
+  @Input("section")
+  public section?: string;
+
+  public rideId?: string;
+
+  constructor(private route: ActivatedRoute)
   {}
+
+  public ngOnInit()
+  {
+    this.route.params.subscribe({
+      next: params =>
+      {
+        this.rideId = params.id;
+      },
+    });
+  }
 }
