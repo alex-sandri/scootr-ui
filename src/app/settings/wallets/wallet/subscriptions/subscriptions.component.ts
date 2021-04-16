f6e9851dd2c87288099f8001d95043d07c276ea5
Alex Sandri <me@alexsandri.com>
2021-04-16 17:55:49 +0200

    Update

--- src/app/settings/wallets/wallet/subscriptions/subscriptions.component.ts
+++ src/app/settings/wallets/wallet/subscriptions/subscriptions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ApiService, ISubscription } from 'src/app/services/api/api.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
@@ -9,47 +10,50 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 })
 export class SubscriptionsComponent implements OnInit
 {
+  private walletId?: string;
+
   public subscriptions?: {
     active: ISubscription[],
     incomplete: ISubscription[],
     old: ISubscription[],
   };
 
-  constructor(private api: ApiService, private auth: AuthService)
+  constructor(private api: ApiService, private route: ActivatedRoute)
   {}
 
   public ngOnInit()
   {
-    if (!this.auth.user)
-    {
-      return;
-    }
-
-    this.api.listSubscriptionsForWallet(this.auth.user.id).then(response =>
-    {
-      if (response.data)
+    this.route.params.subscribe({
+      next: async params =>
       {
-        this.subscriptions = {
-          active: response.data.filter(_ => _.status === "active"),
-          /**
-           * Incomplete subscriptions are the result of failed payments
-           * or payments that require additional steps, such as 3D Secure
-           */
-          incomplete: response.data.filter(_ => _.status !== "active" && !_.deleted),
-          old: response.data.filter(_ => _.deleted),
-        };
-      }
+        this.walletId = params.id;
+
+        const response = await this.api.listSubscriptionsForWallet(params.id);
+
+        if (response.data)
+        {
+          this.subscriptions = {
+            active: response.data.filter(_ => _.status === "active"),
+            /**
+             * Incomplete subscriptions are the result of failed payments
+             * or payments that require additional steps, such as 3D Secure
+             */
+            incomplete: response.data.filter(_ => _.status !== "active" && !_.deleted),
+            old: response.data.filter(_ => _.deleted),
+          };
+        }
+      },
     });
   }
 
   public async manageSubscriptions()
   {
-    if (!this.auth.user)
+    if (!this.walletId)
     {
       return;
     }
 
-    const response = await this.api.createBillingPortalSession(this.auth.user.id);
+    const response = await this.api.createBillingPortalSession(this.walletId);
 
     if (response.data)
     {
