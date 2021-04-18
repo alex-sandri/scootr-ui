3e21b12f0adc9e9d01104964f7494df17792fa04
Alex Sandri <me@alexsandri.com>
2021-04-18 11:09:09 +0200

    Implement method to start export

--- src/app/account/settings/advanced/advanced.component.ts
+++ src/app/account/settings/advanced/advanced.component.ts
@@ -12,6 +12,21 @@ export class AdvancedSettingsComponent
   constructor(private api: ApiService, private auth: AuthService)
   {}
 
+  public async createExport()
+  {
+    if (!this.auth.user)
+    {
+      return;
+    }
+
+    const response = await this.api.createExport(this.auth.user.id);
+
+    if (response.success)
+    {
+      // TODO
+    }
+  }
+
   public async deleteAccount()
   {
     if (!this.auth.user)
